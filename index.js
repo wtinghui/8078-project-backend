d7c3f643f0e025f9289e49e6730aaa85a583b203
wtinghui <weetinghui@gmail.com>
2026-03-28 06:16:17 +0000

    Added routes, data, service layer for books

---
+++ index.js
@@ -0,0 +1,27 @@
+const express= require("express");
+const cors = require("cors");
+require("dotenv").config();
+
+const app = express();
+
+app.use(express.json());
+app.use(cors());
+
+//Routes
+app.get('/',(req,res)=>{
+    res.json({
+        "message":"Test is successful"
+    })
+})
+
+
+//import in the routes
+const bookRoute = require('./routes/bookRoute');
+
+//register the routes
+app.use("/books", bookRoute);
+
+//Start server
+app.listen(process.env.DB_PORT,()=>{
+    console.log("Server has started succcessfully.")
+});
